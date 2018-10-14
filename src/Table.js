db11827e41a7076280dc90618d7e8cad249ef739
Conan1985 <Conan1985@users.noreply.github.com>
2018-10-14 14:19:52 -0400

    Make simpel components

--- src/Table.js
+++ src/Table.js
@@ -1,34 +1,46 @@
 import React from 'react';
 import {Component} from 'react';
 
+const TableHeader = () => {
+    return (
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Job</th>
+        </tr>
+        </thead>
+    )
+};
+
+const TableBody = () => {
+    return (
+        <tbody>
+        <tr>
+            <td>Name1</td>
+            <td>Job1</td>
+        </tr>
+        <tr>
+            <td>Name2</td>
+            <td>Job2</td>
+        </tr>
+        <tr>
+            <td>Name3</td>
+            <td>Job3</td>
+        </tr>
+        <tr>
+            <td>Name4</td>
+            <td>Job4</td>
+        </tr>
+        </tbody>
+    )
+};
+
 class Table extends Component {
     render() {
         return (
             <table>
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Job</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>name1</td>
-                    <td>job1</td>
-                </tr>
-                <tr>
-                    <td>name2</td>
-                    <td>job2</td>
-                </tr>
-                <tr>
-                    <td>name3</td>
-                    <td>job3</td>
-                </tr>
-                <tr>
-                    <td>name4</td>
-                    <td>job4</td>
-                </tr>
-                </tbody>
+                <TableHeader/>
+                <TableBody/>
             </table>
         )
     }
